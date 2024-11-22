3f618662028f3bd39a9368c76b53eeaf56a63db2
Sedky Said <sed@sedkystanleysaid.com>
2024-11-22 07:03:55 +0000

    Update to styles --Restructure classes into correct layers --Adds badges, timeline, form and card classes

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,35 @@
 <script lang="ts"></script>
 
 <main>
-  <h1 class="underline">Hello world!</h1>
-  <h2 class="underline">Hello world!</h2>
-  <h3 class="underline">Hello world!</h3>
-  <h4 class="underline">Hello world!</h4>
-  <h5 class="underline">Hello world!</h5>
   <section class="section">
+    <h1 class="underline">Hello world!</h1>
+    <h2 class="underline">Hello world!</h2>
+    <h3 class="underline">Hello world!</h3>
+    <h4 class="underline">Hello world!</h4>
+    <h5 class="underline">Hello world!</h5>
+  </section>
+  <section class="section">
+    <h2 class="section-header">Section Header</h2>
     <p>This is a paragraph inside of a section.</p>
   </section>
-  <button class="btn-primary" aria-label="Button">Button</button>
-  <button class="btn-secondary" aria-label="Button">Button Secondary</button>
-  <br />
-  <a class="link" href="https://#" aria-label="Link">Link</a>
-  <a class="nav-link" href="https://#" aria-label="Link">Nav Link</a>
+  <section class="section">
+    <button class="btn-primary" aria-label="Button">Button</button>
+    <button class="btn-secondary" aria-label="Button">Button Secondary</button>
+  </section>
+  <section class="section">
+    <a class="link" href="https://#" aria-label="Link">Link</a>
+    <a class="nav-link" href="https://#" aria-label="Link">Nav Link</a>
+  </section>
+  <section class="section">
+    <h2 class="section-header">Footer</h2>
+    <footer class="footer">
+      <nav class="footer-menu">
+        <a href="https://#" aria-label="Link" class="footer-item">Footer item</a
+        >
+      </nav>
+      <p class="footer-icon">Footer icon</p>
+    </footer>
+  </section>
 </main>
 
 <style>
