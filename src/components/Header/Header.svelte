d61f6195ac73943083d0e5e1398aac25e4c1c520
Sedky Said <sed@sedkystanleysaid.com>
2024-11-23 08:46:09 +0000

    Logo image sizes for different resolutions --add sizes for mobile, tablet and desktop

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,11 +1,10 @@
 <script lang="ts" module>
-  import logo from  "../../lib/assets/S-3_logo.svg";
+  import logo from "../../lib/assets/S-3_logo.svg";
   // If screen size greater than 768px, then show header and desktop nav
-
 </script>
 
 <header id="header" class="">
-  <img src={logo} alt="A logo" class="h-auto max-w-12 md: max-w-" />
+  <img src={logo} alt="A logo" class="h-auto sm:max-w-16 md:max-w-28 lg:max-w-36 xl:max-w-36" />
   <nav id="desktop" class="hidden">Desktop Nav</nav>
 </header>
 <nav id="mobile">Mobile Nav</nav>
