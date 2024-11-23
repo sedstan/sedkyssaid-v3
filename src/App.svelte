0b66d2c81f5818a756cee97a6db29be543bdda8c
Sedky Said <sed@sedkystanleysaid.com>
2024-11-23 08:10:31 +0000

    Adds Header component --remove test markup. --change project title --add logo --HTML skeleton

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,10 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import Header from "./components/Header/Header.svelte";
+</script>
 
 <main>
-  <section class="section">
-    <h1 class="underline">Hello world!</h1>
-    <h2 class="underline">Hello world!</h2>
-    <h3 class="underline">Hello world!</h3>
-    <h4 class="underline">Hello world!</h4>
-    <h5 class="underline">Hello world!</h5>
-  </section>
-  <section class="section">
-    <h2 class="section-header">Section Header</h2>
-    <p>This is a paragraph inside of a section.</p>
-  </section>
-  <section class="section">
-    <button class="btn-primary" aria-label="Button">Button</button>
-    <button class="btn-secondary" aria-label="Button">Button Secondary</button>
-  </section>
-  <section class="section">
-    <a class="link" href="https://#" aria-label="Link">Link</a>
-    <a class="nav-link" href="https://#" aria-label="Link">Nav Link</a>
-  </section>
-  <section class="section">
-    <h2 class="section-header">Footer</h2>
-    <footer class="footer">
-      <nav class="footer-menu">
-        <a href="https://#" aria-label="Link" class="footer-item">Footer item</a
-        >
-      </nav>
-      <p class="footer-icon">Footer icon</p>
-    </footer>
-  </section>
+  <Header />
+  <section class="section"></section>
 </main>
 
 <style>
---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,11 @@
+<script lang="ts" module>
+  import logo from  "../../lib/assets/S-3_logo.svg";
+  // If screen size greater than 768px, then show header and desktop nav
+
+</script>
+
+<header id="header" class="">
+  <img src={logo} alt="A logo" class="h-auto max-w-60" />
+  <nav id="desktop" class="">Desktop Nav</nav>
+</header>
+<nav id="mobile">Mobile Nav</nav>
