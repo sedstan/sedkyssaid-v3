ce6c0c60753ab24817dc2a3c656aafeb03532b69
Sedky Said <sed@sedkystanleysaid.com>
2024-11-26 07:01:15 +0000

    WIP - remove mobile classes and add desktop classes.

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -2,9 +2,25 @@
   import logo from "../../lib/assets/S-3_logo.svg";
   // Show header with mobile nav.
   // If if md screen size, then show header with mboile nav and hide desktop nav
+  // When width >1024px, then remove sticky class, remove z-10, remove bottom-0. Add lg:z-0.
+  const screenWidth = window.innerWidth;
+  const headerId = document.getElementById('header');
+  const headerElement = document.querySelector('header');
+
+  screenWidth.addEventListener('resize', () => {
+    const isResized = !isResized;
+    updatedStyles();
+  })
+
+  
   
   // header needs to be sticky
 
+
+
+    function updatedStyles() {
+        throw new Error("Function not implemented.");
+    }
 </script>
 
 <header id="header" class="bg-primary z-10 sticky bottom-0 w-full flex lg:z-0">
